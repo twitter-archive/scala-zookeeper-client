5986837bca7aabf5b7d347420f6d25fad81372e2
Steve Jenson <stevej@twitter.com>
2010-05-28 08:36:37 -0700

    new plugin gives me libs/ and also we can publish to scala-tools

--- project/build/ZookeeperClientProject.scala
+++ project/build/ZookeeperClientProject.scala
@@ -4,10 +4,6 @@ import com.twitter.sbt.{StandardProject, SubversionRepository}
 
 
 class ZookeeperClientProject(info: ProjectInfo) extends StandardProject(info) with SubversionRepository {
-  // Local libraries got into the libs/ directory
-  val local = Resolver.file("libs", new java.io.File("libs")) transactional()
-
-  //override def dependencyPath = "libs"
   // Maven repositories
   val mavenDotOrg = "repo1" at "http://repo1.maven.org/maven2/"
   val jBoss = "jboss-repo" at "http://repository.jboss.org/maven2/"
@@ -33,4 +29,17 @@ class ZookeeperClientProject(info: ProjectInfo) extends StandardProject(info) wi
   val javautils = "org.scala-tools" % "javautils" % "2.7.4-0.1"
   val ostrich = "com.twitter" % "ostrich" % "1.1.6"
   val zookeeper = "org.apache" % "zookeeper" % "3.3.1"
+
+  Credentials(Path.userHome / ".ivy2" / "credentials", log)
+  val publishTo = "nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"
+
+  override def pomExtra =
+    <licenses>
+      <license>
+        <name>Apache 2</name>
+        <url>http://www.apache.org/licenses/LICENSE-2.0.txt</url>
+        <distribution>repo</distribution>
+      </license>
+    </licenses>
+
 }
