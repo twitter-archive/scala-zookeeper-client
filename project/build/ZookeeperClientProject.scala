a86ef585470c68c88de18a7c6e289b33ae41bf57
John Corwin <jcorwin@twitter.com>
2010-11-09 11:56:32 -0800

    get specs from maven

--- project/build/ZookeeperClientProject.scala
+++ project/build/ZookeeperClientProject.scala
@@ -15,11 +15,8 @@ class ZookeeperClientProject(info: ProjectInfo) extends StandardProject(info) wi
   val slf4jApi = "org.slf4j" % "slf4j-api" % "1.5.8"
   val slf4jLog = "org.slf4j" % "slf4j-log4j12" % "1.5.8"
   val log4j = "apache-log4j" % "log4j" % "1.2.15"
-//  val commonsLogging = "commons-logging" % "commons-logging" % "1.1"
   val configgy = "net.lag" % "configgy" % "2.0.1"
-//  val javautils = "org.scala-tools" % "javautils" % "2.7.4-0.1"
-//  val ostrich = "com.twitter" % "ostrich" % "1.2.10"
-//  val zookeeper = "org.apache.zookeeper" % "zookeeper" % "3.3.1"
+  val specs = "org.scala-tools.testing" %% "specs" % "1.6.5" % "test"
 
   Credentials(Path.userHome / ".ivy2" / "credentials", log)
   val publishTo = "nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"
