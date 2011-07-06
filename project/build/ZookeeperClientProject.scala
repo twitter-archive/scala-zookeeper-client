b780917b1367736799788fbbd39c1dfcf05c83e9
Ryan King <ryan@theryanking.com>
2011-07-05 17:44:21 -0700

    let apps pick their logging stuff

--- project/build/ZookeeperClientProject.scala
+++ project/build/ZookeeperClientProject.scala
@@ -7,10 +7,6 @@ class ZookeeperClientProject(info: ProjectInfo) extends StandardLibraryProject(i
   with NoisyDependencies {
 
   val sp = "org.scala-tools.testing" % "specs_2.8.0"  % "1.6.5"
-  val slf4jApi = "org.slf4j" % "slf4j-api" % "1.5.8"
-  val slf4jLog = "org.slf4j" % "slf4j-log4j12" % "1.5.8"
-  val log4j = "log4j" % "log4j" % "1.2.16"
-  val commonsLogging = "commons-logging" % "commons-logging" % "1.1"
   val ostrich = "com.twitter" % "ostrich" % "4.4.0"
   val zookeeper = "org.apache" % "zookeeper" % "3.3.2-dev"
 
