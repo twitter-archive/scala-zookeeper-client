c0ba49f50d4a541ff20f0334e5dac2f8943a25c4
Ryan King <ryan@twitter.com>
2011-07-29 11:14:56 -0700

    update ostrich dep

--- project/build/ZookeeperClientProject.scala
+++ project/build/ZookeeperClientProject.scala
@@ -10,7 +10,7 @@ class ZookeeperClientProject(info: ProjectInfo) extends StandardLibraryProject(i
   override def subversionRepository = Some("http://svn.local.twitter.com/maven/")
 
   val sp = "org.scala-tools.testing" % "specs_2.8.0"  % "1.6.5"
-  val ostrich = "com.twitter" % "ostrich" % "4.4.0"
+  val ostrich = "com.twitter" % "ostrich" % "4.7.3"
   val zk = "org.apache.zookeeper" % "zookeeper" % "3.3.0"
 
   override def ivyXML =
